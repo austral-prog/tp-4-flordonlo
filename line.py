781f9bb904334d9f3a032d4ae0ab6e3038e702a7
flordonlo <162724419+flordonlo@users.noreply.github.com>
2024-04-19 09:14:14 -0300

    Update line.py

--- line.py
+++ line.py
@@ -9,9 +9,9 @@ def line():
     print(f"El coeficiente B de su ecuación de la recta es: {float(coef_B)}")
     print(f"El coeficiente X1 de su ecuación de la recta es: {float(coef_X1)}")
     print(f"El coeficiente X2 de su ecuación de la recta es: {float(coef_X2)}\n")
-    print("Para la siguiente ecuación:")
+    print("Para la siguiente ecuación: ")
     print(f"\t{Y} = {float(coef_A)}{X} + {coef_B}\n")
-    print("Dados los siguientes puntos:")
+    print("Dados los siguientes puntos: ")
     X1= coef_X1
     print(f"\tP1 ({float(coef_X1)}, {float(coef_A*X1+coef_B)})")
     X2= coef_X2
