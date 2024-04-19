fd3dd760b4ed4a2a7c42245cc702d8105ae7214f
flordonlo <162724419+flordonlo@users.noreply.github.com>
2024-04-19 09:00:09 -0300

    Update line.py

--- line.py
+++ line.py
@@ -1,2 +1,25 @@
 def line():
-    print("TO DO")
+    Y= "Y"
+    X= "X"
+    coef_A= float(input("Ingrese el coeficiente A: "))
+    coef_B= float(input("Ingrese el coeficiente B: "))
+    coef_X1= float(input("Ingrese el coeficiente X1: "))
+    coef_X2= float(input("Ingrese el coeficiente X2: "))
+    print(f"El coeficiente A de su ecuación de la recta es: {float(coef_A)}")
+    print(f"El coeficiente B de su ecuación de la recta es: {float(coef_B)}")
+    print(f"El coeficiente X1 de su ecuación de la recta es: {float(coef_X1)}")
+    print(f"El coeficiente X2 de su ecuación de la recta es: {float(coef_X2)}")
+    print(" ")
+    print("Para la siguiente ecuación:")
+    print(f"\t{Y} = {float(coef_A)}{X} + {coef_B}")
+    print(" ")
+    print("Dados los siguientes puntos:")
+    X1= coef_X1
+    print(f"\tP1 ({float(coef_X1)}, {float(coef_A*X1+coef_B)})")
+    X2= coef_X2
+    print(f"\tP2 ({float(coef_X2)}, {float(coef_A*X2+coef_B)})")
+    print(" ")
+    Y1= float(coef_A*X1+coef_B)
+    Y2= float(coef_A*X2+coef_B)
+    print(f"La distancia entre ellos es: {((float(X2-X1))**2+(float(Y2-Y1))**2)**(0.5)}")
+line()
